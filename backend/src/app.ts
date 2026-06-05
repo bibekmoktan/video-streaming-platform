7fee9aeb3a44bb0c48d351c39bd8a0044f86bceb
Bibek Moktan <Bibekmoktan1222@gmail.com>
2026-06-05 21:40:42 +0530

    basic server setup added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,52 @@ import express from 'express';
 
 const app = express();
 
-app.get('/',(req, res) =>{
+app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+
+app.get('/about', (req, res) => {
+    res.json({
+        name: 'Video Streaming Platform',
+        role: 'Backend API',
+    })
+})
+app.get('/contact', (req, res) => {
+    res.json({
+        email: 'bibek@example.com',
+        phone: '1234'
+    })
+})
+
+
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id;
+    res.json({
+        id: userId,
+        name: 'John Doe',
+        email: 'bibek@gmail.com'
+    })
+})
+
+const videos = [
+    {
+        id: 1,
+        title: 'Video 1',
+        description: 'This is video 1'
+    },
+    {
+        id: 2,
+        title: 'Video 2',
+        description: 'This is video 2'
+    },
+]
+
+app.get("/search", (req, res) => {
+  const sarch = req.query.q as string;
+  const results = videos.filter(video => video.title.toLowerCase().includes(sarch.toLowerCase()));
+  res.json(results);
+});
+
+
 
 export default app
